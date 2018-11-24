c2f243bc0dd9387af0aa6bb376d60bee97c6c57b
Christopher Caroni <christophercaroni97@gmail.com>
2018-11-24 18:12:25 +0100

    refactor(Toggle): add animation to icon

--- src/Toggle/Toggle.jsx
+++ src/Toggle/Toggle.jsx
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { VelocityComponent } from 'velocity-react';
 
 import { Add } from '../icons/Add';
 import { Remove } from '../icons/Remove';
 
-const Toggle = ({ className, style, expanded }) => {
+const Toggle = ({ className, style, expanded, getAnimation, duration }) => {
     return (
         <div className={`rtv-toggle ${className}`} style={style}>
-            {expanded ? <Remove /> : <Add />}
+            <VelocityComponent animation={getAnimation(expanded)} duration={duration}>
+                {expanded ? <Remove /> : <Add />}
+            </VelocityComponent>
         </div>
     );
 };
@@ -16,12 +19,18 @@ Toggle.propTypes = {
     className: PropTypes.string,
     style: PropTypes.object,
     expanded: PropTypes.bool,
+    getAnimation: PropTypes.func,
+    duration: PropTypes.number,
 };
 
 Toggle.defaultProps = {
     className: '',
     style: undefined,
     expanded: false,
+    getAnimation: expanded => ({
+        rotateZ: expanded ? 180 : 360,
+    }),
+    duration: 100,
 };
 
 export { Toggle };
--- src/Toggle/Toggle.stories.jsx
+++ src/Toggle/Toggle.stories.jsx
@@ -3,6 +3,26 @@ import React from 'react';
 
 import { Toggle } from './Toggle';
 
+class StatefulToggle extends React.Component {
+    state = {
+        expanded: false,
+    };
+
+    toggle = () => this.setState(({ expanded }) => ({ expanded: !expanded }));
+
+    render() {
+        const { expanded } = this.state;
+
+        return (
+            <div onClick={this.toggle} style={{ cursor: 'pointer' }}>
+                <div>click me to trigger the animation</div>
+                <Toggle expanded={expanded} />
+            </div>
+        );
+    }
+}
+
 storiesOf('Toggle')
     .add('default (expand)', () => <Toggle />)
-    .add('hide', () => <Toggle expanded />);
+    .add('hide', () => <Toggle expanded />)
+    .add('toggle example', () => <StatefulToggle />);
