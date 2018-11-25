24a0fffce695844d76f6193cad5ebb291c717525
Christopher Caroni <christophercaroni97@gmail.com>
2018-11-25 15:25:28 +0100

    core: export components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { Tree } from './Tree';
+
+export * from './Cell';
+export * from './Header';
+export * from './icons/Add';
+export * from './icons/Remove';
+export * from './Toggle';
+export * from './Tree';
+export * from './animations';
+export * from './proptypes';
+
+export default Tree;
