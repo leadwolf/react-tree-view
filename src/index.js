40cbcc88848563bdef342d36af5c460cde7458f6
Christopher Caroni <christophercaroni97@gmail.com>
2018-11-25 12:58:45 +0100

    delete old components

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { Container } from './Container';
-import { Header } from './Header';
-import { Node } from './Node';
-import { Tree } from './Tree';
-import { Toggle } from './Toggle';
-
-export { Tree as default, Tree, Node, Container, Toggle, Header };
