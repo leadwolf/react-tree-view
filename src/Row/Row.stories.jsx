bda26668c97c375f82c6aea0df488db03dbf7c39
Christopher Caroni <christophercaroni97@gmail.com>
2018-11-25 14:44:05 +0100

    stories(Row): add table example

--- src/Row/Row.stories.jsx
+++ src/Row/Row.stories.jsx
@@ -1,3 +1,5 @@
+import './rowStories.scss';
+
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
@@ -6,7 +8,14 @@ import { Toggle } from '../Toggle';
 import { Row } from './Row';
 import { WithExpandedRowIdsState } from '../stories/WithExpandedRowIdsState';
 
+const Cell = ({ style, children }) => (
+    <div className="story-row-table-content-cell" style={style}>
+        {children}
+    </div>
+);
+
 storiesOf('Row', module)
+    .addDecorator(storyFn => <div className="story">{storyFn()}</div>)
     .add('default: !expanded', () => <Row />)
     .add('with node', () => <Row node={dummyNode} />)
     .add('with renderContent', () => (
@@ -33,6 +42,44 @@ storiesOf('Row', module)
                             >
                                 {node.id}
                             </div>
+                        </React.Fragment>
+                    )}
+                />
+            )}
+        </WithExpandedRowIdsState>
+    ))
+    .add('table example', () => (
+        <WithExpandedRowIdsState>
+            {(expandedRowIds, toggle) => (
+                <Row
+                    onClick={toggle}
+                    node={dummyNode}
+                    expandedRowIds={expandedRowIds}
+                    classes={{ content: 'story-row-table-content' }}
+                    noIndent
+                    renderContent={({ node, hasChildren, indentLeft }) => (
+                        <React.Fragment>
+                            <Cell
+                                style={{
+                                    paddingLeft: `${indentLeft}px`,
+                                }}
+                            >
+                                {hasChildren && (
+                                    <Toggle expanded={Boolean(expandedRowIds[node.id])} />
+                                )}
+
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'center',
+                                        paddingLeft: '10px',
+                                    }}
+                                >
+                                    {node.id}
+                                </div>
+                            </Cell>
+                            <Cell>{node.name}</Cell>
                         </React.Fragment>
                     )}
                 />
